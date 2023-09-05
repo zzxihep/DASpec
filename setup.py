497395199327fde0f266ebdb88fb999e90f34ca6
zzx <zzx@bigtree.xyz>
2023-09-05 15:42:15 +0800

    update the setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 curdir = os.getcwd()
 sdir = os.path.dirname(os.path.abspath(__file__))
-mpdir = os.path.join([sdir, 'mpfit/cmpfit-1.3a'])
+mpdir = os.path.join(sdir, 'mpfit/cmpfit-1.3a')
 os.chdir(mpdir)
 os.system('make')
 os.chdir(curdir)
-DAS_dir = os.path.join([sdir, 'src_swig'])
+DAS_dir = os.path.join(sdir, 'DASpec')
 os.chdir(DAS_dir)
 os.system('make')
 os.chdir(curdir)
